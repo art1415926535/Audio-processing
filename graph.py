098facbfd2f7c426314699609dc1cd49366124d0
Artem <art1415926535@gmail.com>
2015-01-31 15:43:51 +0300

    Добавил тесты

--- graph.py
+++ graph.py
@@ -1,11 +1,34 @@
+import datetime
+
 import matplotlib.pyplot as plt
 
-z = [[1, 2, 3, 4],
-     [2, 3, 4, 5]]
 
-plt.axes().get_xaxis().set_visible(False)  # del labels
-plt.axes().get_yaxis().set_visible(False)  # del labels
-plt.tight_layout()  # del gray area around plot
+def new_graph(array, path='/img/', name=None):
+    if name is None:
+        name = datetime.datetime.now().time().isoformat()[:-7]  # -msec
 
-plt.imshow(z)
-plt.savefig('foo.png', bbox_inches='tight', pad_inches=0)
+    plt.axes().get_xaxis().set_visible(False)  # del labels
+    plt.axes().get_yaxis().set_visible(False)  # del labels
+    plt.tight_layout()  # del gray area around plot
+
+    plt.imshow(array, interpolation='none')
+
+    p = path + name + '.png'
+    plt.savefig(p, bbox_inches='tight', pad_inches=0)
+
+    return p
+
+
+if __name__ == '__main__':
+    import random
+    import time
+
+    t_rand = time.time()
+    z = [[random.randint(0, 30) for _ in range(1000)],
+         [random.randint(0, 30) for _ in range(1000)]] * 2000
+    print("t rand:", time.time() - t_rand)
+    print(len(z), len(z[0]))
+
+    t_graph = time.time()
+    new_graph(z, '', 'foo')
+    print('time creating graph:', time.time() - t_graph)
