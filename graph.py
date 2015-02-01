ca8b384a45d2d3f6ccaea23413da6baa66a9da86
Artem <art1415926535@gmail.com>
2015-02-01 15:24:55 +0300

    Разочарование в matplotlib и запил своего на pillow

--- graph.py
+++ graph.py
@@ -1,9 +1,11 @@
 import datetime
+from logging import critical
 
 import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw
 
 
-def new_graph(array, path='/img/', name=None):
+def create_2d_graph(array, path='/img/', name=None):
     if name is None:
         name = datetime.datetime.now().time().isoformat()[:-7]  # -msec
 
@@ -11,24 +13,51 @@ def new_graph(array, path='/img/', name=None):
     plt.axes().get_yaxis().set_visible(False)  # del labels
     plt.tight_layout()  # del gray area around plot
 
-    plt.imshow(array, interpolation='none')
+    plt.plot(array)  # , interpolation='none')
 
     p = path + name + '.png'
-    plt.savefig(p, bbox_inches='tight', pad_inches=0)
+    plt.savefig(p, dpi=100, bbox_inches='tight', pad_inches=0)
 
     return p
 
+
+def create_3d_graph(array, path='/img/', name=None):
+    try:
+        width = len(array)
+        height = len(array[0])
+    except TypeError:
+        critical('NEED 2D ARRAY')
+        return 1
+
+    # create image
+    image = Image.new("RGBA", (width, height), (0, 0, 0, 0))
+    draw = ImageDraw.Draw(image)
+
+    del draw
+
+    # save image
+    if name is None:
+        name = datetime.datetime.now().time().isoformat()[:-7]  # -msec
+    full_path = path + name + '.png'
+    image.save(full_path, "PNG")
+
+    return full_path
 
 if __name__ == '__main__':
     import random
     import time
 
+    '''
     t_rand = time.time()
-    z = [[random.randint(0, 30) for _ in range(1000)],
-         [random.randint(0, 30) for _ in range(1000)]] * 2000
+    width = 300
+    height = 900
+    z = [[random.randint(0, 30) for _ in range(width)]] * (height // 3) + \
+        [[random.randint(0, 30) for _ in range(width)]] * (height // 3) + \
+        [[random.randint(0, 30) for _ in range(width)]] * (height // 3)
     print("t rand:", time.time() - t_rand)
     print(len(z), len(z[0]))
-
+    '''
+    z = [random.randint(0, 100) / 100 * x for x in range(2000)]
     t_graph = time.time()
-    new_graph(z, '', 'foo')
+    create_2d_graph(z, '', 'foo')
     print('time creating graph:', time.time() - t_graph)
