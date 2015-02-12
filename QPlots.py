1ad668df49372607cd7a79fc2aa17b98c63b8a5c
Artem <art1415926535@gmail.com>
2015-02-13 00:21:00 +0300

    Декоратор на тексты. (наркоман?)

--- QPlots.py
+++ QPlots.py
@@ -1,6 +1,28 @@
+from time import time
+
 from PyQt5.QtWidgets import QWidget, QFrame, QSizePolicy
 from PyQt5.QtGui import QImage, QPainter, QPen, QColor
-from PyQt5.QtCore import QByteArray
+from PyQt5.QtCore import QByteArray, Qt
+
+
+def test_spectrum(function):
+    def decorated(self, *args, **kwargs):
+        count_numbers = 10000
+        max_number = 10
+        data = []
+        import random
+
+        for i in range(count_numbers):
+            data += [[x * random.normalvariate(0.9, 0.1) for x in range(max_number)]] * (count_numbers // count_numbers)
+
+        t = time()
+        function(self, data, max_number)
+        print('>>> Время выполнения функции: {}\n>>> Количество значений: {}'.format(
+            time() - t,
+            count_numbers * max_number)
+        )
+
+    return decorated
 
 
 class Plots(QFrame):
@@ -8,60 +30,45 @@ class Plots(QFrame):
         QWidget.__init__(self, parent)
 
         self.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+        self.setMinimumHeight(42)
+        self.setMaximumHeight(9000)
+
         self.width_border = 0
         self.background = '#f6f6f6'
         self.setStyleSheet('border: {}px solid color; background-color:{}'.format(self.width_border,
                                                                                   self.background))
 
-        self.__number_of_plots = 0
-        self.__plots = []
+        self.__plot = QImage()
+        self.src = QImage()
         self.__percent = 0
-        self.__width_of_images = 0
+        self.q_byte_array = QByteArray()
 
-    def add_data(self, data=None, width=100, height=100, number=None):
-
-        z_points = 1000
-        z = []
-
-        # n = random.randint(z_points / 5, z_points)
-        n = z_points
-        for i in range(n):
-            # z += [[random.randint(0, 100) / 100 * x for x in range(200)]] * (z_points // n)
-            z += [[x for x in range(200)]] * (z_points // n)
-
-        data = z
-        q_byte_array = QByteArray()
-        print(len(data), len(data[0]))
-
-        from time import time
-
-        t = time()
-
+    @test_spectrum
+    def add_data(self, data=None, max_number=50, height=100):
         if type(data[0]) == list:
             for row in range(len(data[0])):
                 for cell in range(len(data)):
-                    rgba = self.__rgba(data[cell][row], 200)
+                    rgba = self.__rgba(data[cell][row], max_number)
                     for c in rgba[::-1]:
-                        q_byte_array.append(chr(c))
-
-        print('Create:', time() - t)
-
-        graph = QImage(q_byte_array, len(data), len(data[0]), 6)
-
-        if number is None:
-            self.__plots.append(graph)
+                        self.q_byte_array.append(chr(c))
         else:
-            self.__plots.insert(number, graph)
+            assert AttributeError
 
+        self.src = QImage(self.q_byte_array, len(data), len(data[0]), 6)
+
+        self.__plot = self.src
         self.repaint()
 
-    def del_data(self, i):
-        self.__plots.pop(i)
+    def del_data(self):
+        self.__plot = QImage()
         self.repaint()
 
     def update(self, percent=0, *__args):
         self.percent = percent
         super().update(*__args)
+
+    def resizeEvent(self, *args, **kwargs):
+        self.__plot = self.src.scaled(self.__plot.width(), self.height())
 
     def paintEvent(self, *__args):
         painter = QPainter(self)
@@ -69,14 +76,13 @@ class Plots(QFrame):
         painter.setPen(QPen(QColor.blue))
         accumulator_y = 0
 
-        for i in range(len(self.__plots)):
-            painter.drawImage(self.width() // 2 - self.__plots[i].width() * self.percent, accumulator_y,
-                              self.__plots[i])
-            accumulator_y += self.__plots[i].height()
+        painter.drawImage(self.width() // 2 - self.__plot.width() * self.percent, accumulator_y,
+                          self.__plot)
+        accumulator_y += self.__plot.height()
 
         delta_x = 0
         for aa in range(255, 0, -20):
-            painter.setPen(QPen(QColor(255, 200, 200, aa)))
+            painter.setPen(QPen(QColor(0, 0, 0, aa)))
             painter.drawLine(self.width() // 2 - delta_x, 0, self.width() // 2 - delta_x, self.height())
             delta_x += 1
 
@@ -91,8 +97,8 @@ class Plots(QFrame):
         else:
             raise IndexError
 
-    def __rgba(self, x, max_z):
-        x = int(x / max_z * 765)
+    def __rgba(self, x, max_x):
+        x = int(x / max_x * 765)
         # r  g  b
         color = [1, 1, 1]
         for i in (2, 0, 1):
@@ -107,3 +113,29 @@ class Plots(QFrame):
             color[2] = 256 - color[0]
 
         return [255] + color
+
+    def wheelEvent(self, event):
+        print(event.angleDelta())
+        if 20 < self.minimumHeight() < self.parent().height() - 200 or \
+                (event.angleDelta().y() < 0 and self.minimumHeight() >= self.parent().height()):
+            self.setMinimumHeight(self.height() + event.angleDelta().y() / 24)
+            self.setMaximumHeight(self.height() + event.angleDelta().y() / 24)
+        elif self.maximumHeight() > 50 or event.angleDelta().y() > 0:
+            self.setMinimumHeight(self.height() + event.angleDelta().y() / 24)
+            self.setMaximumHeight(self.height() + event.angleDelta().y() / 24)
+
+    def mouseReleaseEvent(self, mouse_event):
+        if mouse_event.button() == Qt.MidButton:
+            self.setMinimumHeight(42)
+            self.setMaximumHeight(9000)
+
+        elif mouse_event.button() == Qt.LeftButton and self.height() < 500:
+            self.setMinimumHeight(self.height() + 100)
+            self.setMaximumHeight(self.height() + 100)
+
+        elif mouse_event.button() == Qt.RightButton and self.height() > 142:
+            self.setMinimumHeight(self.height() - 100)
+            self.setMaximumHeight(self.height() - 100)
+
+
+
