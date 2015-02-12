9ff041b875225fdd30b59a5ff2fe7e635df71d18
Artem <art1415926535@gmail.com>
2015-02-12 16:39:18 +0300

    Долгое создание спектра

--- QPlots.py
+++ QPlots.py
@@ -19,38 +19,34 @@ class Plots(QFrame):
         self.__width_of_images = 0
 
     def add_data(self, data=None, width=100, height=100, number=None):
-        '''
-        array = data
-        # FIXME переделать массив в нормальный 0xAARRGGBB
-        graph = QImage(array,
-                       width=width,
-                       height=height,
-                       QImage.Format_ARGB32_Premultiplied)
-        '''  # TODO: затычко
-        z_points = 10000
-        z = []
-        import random
 
-        n = random.randint(z_points / 5, z_points)
+        z_points = 1000
+        z = []
+
+        # n = random.randint(z_points / 5, z_points)
+        n = z_points
         for i in range(n):
-            z += [[random.randint(0, 100) / 100 * x for x in range(200)]] * (z_points // n)
+            # z += [[random.randint(0, 100) / 100 * x for x in range(200)]] * (z_points // n)
+            z += [[x for x in range(200)]] * (z_points // n)
 
         data = z
         q_byte_array = QByteArray()
         print(len(data), len(data[0]))
+
+        from time import time
+
+        t = time()
+
         if type(data[0]) == list:
             for row in range(len(data[0])):
                 for cell in range(len(data)):
-                    rgba = self.__rgba(data[cell][row], 100)
+                    rgba = self.__rgba(data[cell][row], 200)
                     for c in rgba[::-1]:
-                        if not c:
-                            q_byte_array.append(chr(1))
-                        else:
-                            q_byte_array.append(chr(c))
+                        q_byte_array.append(chr(c))
 
-        graph = QImage(q_byte_array, len(data), len(data[0]), 5)
-        print(graph.height(), graph.width(), graph.bytesPerLine())
+        print('Create:', time() - t)
 
+        graph = QImage(q_byte_array, len(data), len(data[0]), 6)
 
         if number is None:
             self.__plots.append(graph)
@@ -71,12 +67,7 @@ class Plots(QFrame):
         painter = QPainter(self)
         painter.setRenderHint(QPainter.Antialiasing)
         painter.setPen(QPen(QColor.blue))
-        # painter.drawLine(self.width()*self.percent, self.height()*self.percent,
-        # self.width()*(1-self.percent), self.height()*(1-self.percent))
-        #
-        # painter.drawLine(self.width()*(1-self.percent), self.height()*self.percent,
-        #                  self.width()*self.percent, self.height()*(1-self.percent))
-        accumulator_y = 10
+        accumulator_y = 0
 
         for i in range(len(self.__plots)):
             painter.drawImage(self.width() // 2 - self.__plots[i].width() * self.percent, accumulator_y,
@@ -85,7 +76,7 @@ class Plots(QFrame):
 
         delta_x = 0
         for aa in range(255, 0, -20):
-            painter.setPen(QPen(QColor(50, 130, 150, aa)))
+            painter.setPen(QPen(QColor(255, 200, 200, aa)))
             painter.drawLine(self.width() // 2 - delta_x, 0, self.width() // 2 - delta_x, self.height())
             delta_x += 1
 
@@ -101,21 +92,18 @@ class Plots(QFrame):
             raise IndexError
 
     def __rgba(self, x, max_z):
-        x = x / max_z * (255 * 3)
-        # r  g  b   a
-        color = [0, 0, 0]
-        for i in [2, 0, 1]:
-            if x > 0:
-                if x > 255:
-                    color[i] = int(255)
-                elif x < 0:
-                    color[i] = int(0)
-                else:
-                    color[i] = int(x)
+        x = int(x / max_z * 765)
+        # r  g  b
+        color = [1, 1, 1]
+        for i in (2, 0, 1):
+            if x > 255:
+                color[i] = 255
+            elif x > 0:
+                color[i] = x
 
-                x -= 255
+            x -= 255
 
-            if color[0] > 0:
-                color[2] = 255 - color[0]
+        if color[0] > 1:
+            color[2] = 256 - color[0]
 
         return [255] + color
