8fba7aee42f0024859b6feb4f8b98c6196dc3a98
Artem <art1415926535@gmail.com>
2015-03-04 10:34:37 +0300

    QCodeEdit с методами сохранения и загрузки нескольких исходников и переключения между ними.

---
+++ QCodeEdit.py
@@ -0,0 +1,69 @@
+from PyQt5.QtWidgets import QPlainTextEdit, QSizePolicy, QFileDialog, QMessageBox
+
+import styleSheets
+
+
+class QCodeEdit(QPlainTextEdit):
+    def __init__(self, *__args):
+        super().__init__(*__args)
+        self.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+        self.setStyleSheet(styleSheets.QCodeEdit)
+
+        self.loaded_files = []  # [[name of file, text of file], ...]
+        self.now_file = 0
+
+    def new_file(self):
+        import datetime
+
+        self.loaded_files.append({'name': 'Algorithm {}'.format(len(self.loaded_files)),
+                                  'path': None,
+                                  'text': '{date}\n\nimport calc\n\n'.format(
+                                      date=datetime.datetime.now().isoformat(' ')[:-7])})
+
+        self.setPlainText(self.loaded_files[-1]['text'])
+        return self.loaded_files[-1]['name']
+
+    def open_file(self):
+        path, extension = QFileDialog.getOpenFileName(self, 'Open algorithm', '', filter='*.py')
+        if path:
+            with open(path, 'r') as f:
+                self.setPlainText(f.read())
+
+            name_of_file = path[-path[::-1].find('/'):]
+            self.loaded_files.append({'name': name_of_file,
+                                      'path': path,
+                                      'text': self.toPlainText()})
+            self.now_file = len(self.loaded_files) - 1
+            return name_of_file
+
+    def load_file(self, name):
+        self.loaded_files[self.now_file]['text'] = self.toPlainText()
+
+        for i in range(len(self.loaded_files)):
+            if self.loaded_files[i]['name'] in name:
+                self.now_file = i
+                self.setPlainText(self.loaded_files[i]['text'])
+                break
+
+    def save_file(self):
+        self.loaded_files[self.now_file]['text'] = self.toPlainText()
+
+        if self.loaded_files[self.now_file]['path'] is None:
+            path, extension = QFileDialog.getSaveFileName(self, 'Save algorithm', '', filter='*.py')
+            if path[:-3] not in extension[1:]:
+                path += extension[1:]
+            self.loaded_files[self.now_file]['path'] = path
+
+        with open(self.loaded_files[self.now_file]['path'], 'w') as f:
+            f.write(self.toPlainText())
+
+        message_box = QMessageBox()
+        message_box.setWindowTitle('Save')
+        message_box.setText('\nAlgorithm save to' + self.loaded_files[self.now_file]['path'] + '\n')
+        message_box.addButton(QMessageBox.Ok)
+        message_box.exec_()
+
+    def textChanged(self, *args, **kwargs):
+        # self.loaded_files[self.now_file]['text'] = self.toPlainText()
+        print(self.now_file, 'save ')
+
