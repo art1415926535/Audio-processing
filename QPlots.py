794320d91ae64727a8b5134da85e8305a30b0908
Artem <art1415926535@gmail.com>
2015-03-06 14:03:56 +0300

    add pyqtgraph

--- QPlots.py
+++ QPlots.py
@@ -1,8 +1,12 @@
 from time import time
+import random  # FIXME DELETE
+import math
 
 from PyQt5.QtWidgets import QWidget, QFrame, QSizePolicy, QApplication
 from PyQt5.QtGui import QImage, QPainter, QPen, QColor
 from PyQt5.QtCore import QByteArray, Qt
+from pyqtgraph.widgets.GraphicsLayoutWidget import GraphicsLayoutWidget
+from pyqtgraph.graphicsItems import InfiniteLine
 
 import styleSheets
 
@@ -33,25 +37,7 @@ def test_spectrum(function):
     return decorated
 
 
-def rgba(x, max_x):
-    x = x * 765 // max_x
-    # r  g  b
-    color = [1, 1, 1]
-    for i in (2, 0, 1):
-        if x > 255:
-            color[i] = 255
-        elif x > 0:
-            color[i] = x
-
-        x -= 255
-
-    if color[0] > 1:
-        color[2] = 256 - color[0]
-
-    return [255] + color
-
-
-class Plot(QFrame):
+class Plot3d(QFrame):
     def __init__(self, parent=None):
         QWidget.__init__(self, parent)
 
@@ -59,7 +45,7 @@ class Plot(QFrame):
         self.setMinimumHeight(42)  # MAGIC!
         self.setMaximumHeight(9000)  # OVERMAGIC!
 
-        self.background = '#f6f6f6'
+        self.background = '#fff'
         self.setStyleSheet(styleSheets.qplots(self.background))
         self.q_byte_array = QByteArray()
         self.filling = 0
@@ -68,21 +54,39 @@ class Plot(QFrame):
         self.src = QImage()
         self.__percent = 0
 
-    @test_spectrum
-    def add_data(self, data=None, max_number=50, height=100):
-        if type(data[0]) == list:
-            for row in range(len(data[0])):
-                for cell in range(len(data)):
-                    rgba_c = rgba(data[cell][row], max_number)
-                    for c in rgba_c[::-1]:
-                        self.q_byte_array.append(chr(c))
-        else:
-            assert AttributeError
+    @staticmethod
+    def rgba(x, max_x):
+        x = x * 765 // max_x
+        # r  g  b
+        color = [1, 1, 1]
+        for i in (2, 0, 1):
+            if x > 255:
+                color[i] = 255
+            elif x > 0:
+                color[i] = x
 
-        self.src = QImage(self.q_byte_array, len(data), len(data[0]), 6)
+            x -= 255
 
-        self.__plot = self.src
-        self.repaint()
+        if color[0] > 1:
+            color[2] = 256 - color[0]
+
+        return chr(color[2]) + chr(color[1]) + chr(color[0]) + chr(255)
+
+    # @test_spectrum
+    def set_data(self, data=None, max_number=50, height=100):
+        if data is not None:
+            if type(data[0]) == list:
+                for row in range(len(data[0])):
+                    for cell in range(len(data)):
+                        rgba_c = self.rgba(data[cell][row], max_number)
+                        self.q_byte_array.append(rgba_c)
+                self.src = QImage(self.q_byte_array, len(data), len(data[0]), 6)
+
+                self.__plot = self.src
+                self.repaint()
+            else:
+                assert AttributeError
+
 
     def append_q_bate_array(self, c):
         self.q_byte_array.append(c)
@@ -90,7 +94,6 @@ class Plot(QFrame):
         self.src = QImage(self.q_byte_array, len(data), len(data[0]), 6)
         self.__plot = self.src
         self.repaint()
-
 
     def del_data(self):
         self.__plot = QImage()
@@ -153,12 +156,61 @@ class Plot(QFrame):
         self.repaint()
 
 
+class Plot2d(GraphicsLayoutWidget):
+    def __init__(self, parent=None, name=''):
+        QWidget.__init__(self, parent)
+        GraphicsLayoutWidget.__init__(self)
+
+        self.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+        self.setMinimumHeight(42)  # MAGIC!
+        self.setMaximumHeight(9000)  # OVERMAGIC!
+
+        self.background = '#f6f6f6'
+        self.setStyleSheet(styleSheets.qplots(self.background))
+
+        self.__plot = self.addPlot(title=name)
+        self.curve = self.__plot.plot(pen='w')
+        self.line = InfiniteLine.InfiniteLine((0, 0))
+        self.__plot.addItem(self.line)
+        self.data = []
+        self.__percent = 0
+
+    @property
+    def percent(self):
+        return self.__percent
+
+    @percent.setter
+    def percent(self, p):
+        if 0 <= p <= 100:
+            self.__percent = p
+        else:
+            raise IndexError
+
+    def update(self, p=0):
+        self.line.setPos(p * len(self.data))
+
+    def set_data(self, data=[math.sin(d / 30) * random.normalvariate(0.9, 0.1)
+                             for d in range(500)]):
+        self.data = data
+        self.curve.setData(self.data)
+
+
+    def wheelEvent(self, event):
+        if 20 < self.minimumHeight() < self.parent().height() - 200 or \
+                (event.angleDelta().y() < 0 and self.minimumHeight() >= self.parent().height()):
+            self.setMinimumHeight(self.height() + event.angleDelta().y() / 24)
+            self.setMaximumHeight(self.height() + event.angleDelta().y() / 24)
+        elif self.maximumHeight() > 50 or event.angleDelta().y() > 0:
+            self.setMinimumHeight(self.height() + event.angleDelta().y() / 24)
+            self.setMaximumHeight(self.height() + event.angleDelta().y() / 24)
+
 if __name__ == "__main__":
     import sys
 
     app = QApplication(sys.argv)
-    window = Plot()
+    window = Plot2d()
     window.show()
-    window.add_data('lalka')
+
+    window.set_data()
     # window.close()
     app.exec()
